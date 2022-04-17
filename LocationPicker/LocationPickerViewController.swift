31a9d6394d66088ac532a6c4db0b67c115cdea6f
Izumi Higa <higalex@gmail.com>
2022-04-17 19:19:52 -0400

    Make 'Select' button medium weight

--- LocationPicker/LocationPickerViewController.swift
+++ LocationPicker/LocationPickerViewController.swift
@@ -407,6 +407,7 @@ extension LocationPickerViewController: MKMapViewDelegate {
 	func selectLocationButton() -> UIButton {
 		let button = UIButton(frame: CGRect(x: 0, y: 0, width: 70, height: 30))
 		button.setTitle(selectButtonTitle, for: UIControl.State())
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium)
         if let titleLabel = button.titleLabel {
             let width = titleLabel.textRect(forBounds: CGRect(x: 0, y: 0, width: Int.max, height: 30), limitedToNumberOfLines: 1).width
             button.frame.size = CGSize(width: width, height: 30.0)
